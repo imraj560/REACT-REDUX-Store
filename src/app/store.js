cde768c1b2089be97d18dc146fe71c24e5280748
imraj560 <raju560.webdev@gmail.com>
2023-09-15 17:01:10 +0600

    integrated local storage to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,26 @@ import { configureStore } from '@reduxjs/toolkit';
 import  userReducer  from '../features/userSlice';
 import moviesReducer from '../features/movieSlice';
 import cartReducer from '../features/cartSlice';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+import { combineReducers } from '@reduxjs/toolkit';
 
+const persistConfig = {
+  key:"root",
+  verion:1,
+  storage
+};
 
-export const store = configureStore({
-  reducer: {
-    
-    user: userReducer,
+const reducer = combineReducers({
+
+  user: userReducer,
     movies: moviesReducer,
     cart: cartReducer
-  },
+
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer:persistedReducer
 });
